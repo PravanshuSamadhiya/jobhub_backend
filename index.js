107987f437d9b88fa79abf82797ad9c5bc557151
Abhi samadhiya <abhisamadhiya@Abhis-MacBook-Air.local>
2024-08-21 15:06:48 +0530

    your updated backend code

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin: ['https://jobhub-omega.vercel.app', 'http://localhost:5173'],
-    credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 const PORT = process.env.PORT ||  3000;
